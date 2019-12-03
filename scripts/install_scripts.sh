c5b3caacf6955b11622075cf4039e1c728020564
Jintack Lim <jintack@cs.columbia.edu>
2019-12-03 11:58:59 -0800

    Make a file for the QEMU bridged networking
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- scripts/install_scripts.sh
+++ scripts/install_scripts.sh
@@ -4,3 +4,6 @@ LIST='build-n-install.sh copy-kernel.sh pin_vcpus.sh qmp.py qmp-cpus'
 DST=/usr/local/bin
 cp $LIST $DST
 
+mkdir -p /usr/local/etc/qemu/
+echo "allow br0" >  /usr/local/etc/qemu/bridge.conf
+
