4ad76bb86a4b20ea3c23b2d38d106fb6d664f86b
Jintack Lim <jintack@cs.columbia.edu>
2019-12-03 16:06:17 -0800

    Hackbench nits
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- scripts/hackbench.sh
+++ scripts/hackbench.sh
@@ -7,5 +7,5 @@ source setup.sh
 REPTS=${1-4}
 
 for i in `seq 1 $REPTS`; do
-	../$TOOLS/hackbench 100 process 500 | tee >(grep 'Time:' | awk '{ print $2 }' >> $TIMELOG)
+	./hackbench 100 process 500 | tee >(grep 'Time:' | awk '{ print $2 }' >> $TIMELOG)
 done
