457ae73d87a9a97a57e1e5fbfbb6aeb9d752f1d7
Jintack Lim <jintack@cs.columbia.edu>
2019-12-04 19:30:21 -0800

    fixup! Add qemu-ifdown

--- scripts/install_scripts.sh
+++ scripts/install_scripts.sh
@@ -3,7 +3,7 @@
 LIST='build-n-install.sh copy-kernel.sh pin_vcpus.sh qmp.py qmp-cpus'
 DST=/usr/local/bin
 cp $LIST $DST
-cp qemu-ifup /etc
+cp qemu-ifup qemu-ifdown /etc
 
 mkdir -p /usr/local/etc/qemu/
 echo "allow br0" >  /usr/local/etc/qemu/bridge.conf
