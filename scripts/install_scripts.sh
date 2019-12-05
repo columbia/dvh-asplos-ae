b61d8b36fa0fee955084ca605eab126419584103
Jintack Lim <jintack@cs.columbia.edu>
2019-12-04 17:44:52 -0800

    copy qemu-ifup script
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- scripts/install_scripts.sh
+++ scripts/install_scripts.sh
@@ -3,6 +3,7 @@
 LIST='build-n-install.sh copy-kernel.sh pin_vcpus.sh qmp.py qmp-cpus'
 DST=/usr/local/bin
 cp $LIST $DST
+cp qemu-ifup /etc
 
 mkdir -p /usr/local/etc/qemu/
 echo "allow br0" >  /usr/local/etc/qemu/bridge.conf
