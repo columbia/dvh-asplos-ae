658d924790bd08921f13301c4d8b50b4a5bf9f9a
Jintack Lim <jintack@cs.columbia.edu>
2019-12-05 12:55:23 -0800

    Y to everything
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -2,7 +2,6 @@
 echo 'deb http://us.archive.ubuntu.com/ubuntu/ trusty multiverse' >> /etc/apt/sources.list
 echo 'deb-src http://us.archive.ubuntu.com/ubuntu/ trusty multiverse' >> /etc/apt/sources.list
 apt-get update
-apt-get build-dep qemu
-apt-get build-dep linux
+apt-get -y build-dep qemu linux
 apt-get -y install bridge-utils python-pip pbzip2 python-numpy
 pip install 'pexpect==3.1' --force-reinstall
